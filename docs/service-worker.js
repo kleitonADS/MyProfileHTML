ef4519e6bc7791fdae87428521c518aa748c9bc0
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2018-01-15 13:28:18 -0500

    commit

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -62,7 +62,6 @@ self.addEventListener('fetch', event => {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
-          va
           return cachedResponse;
         }
 
