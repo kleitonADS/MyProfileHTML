fa97e210928ab3c0b54e0aa8d4a0ff3f742d2840
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2018-01-15 11:32:39 -0500

    test

--- _assents/js/service-worker.js
+++ _assents/js/service-worker.js
@@ -19,8 +19,8 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-    '/',
-    './index.html',
+    // '/',
+    '../index.html',
     // 'index-br.html',
     // 'manifest.json',
     // 'css/main.css',
