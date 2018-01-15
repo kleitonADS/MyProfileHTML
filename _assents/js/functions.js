33805bd3a88e6e4a2aab91998cdf61cc819056a3
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2018-01-15 14:13:05 -0500

    change service-worker

--- _assents/js/functions.js
+++ _assents/js/functions.js
@@ -8,13 +8,13 @@ $('.list_tool_feedback').hide();
 $('.list_tool_basefe').hide();
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }).catch(function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+  navigator.serviceWorker
+  .register('./service-worker.js', {scope: './'})
+  .then(function(reg) {
+    // registration worked
+    console.log('Registration succeeded. Scope is ' + reg.scope);
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
   });
 }
