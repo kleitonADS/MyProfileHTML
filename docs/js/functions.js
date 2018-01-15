a0f69a89e0a67d1c77c11aeef4b4ac150bf39811
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2018-01-15 10:28:18 -0500

    add service work.js

--- docs/js/functions.js
+++ docs/js/functions.js
@@ -6,3 +6,9 @@ $('.rightlist').hide();
 $('#form-main').hide();
 $('.list_tool_feedback').hide();
 $('.list_tool_basefe').hide();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
